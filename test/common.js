3a7a68d78928ec97258c0895919e419c23e32eda
Frank <git@nocke.de>
2017-08-03 18:00:02 +0200

    minor clean

--- test/common.js
+++ test/common.js
@@ -1,8 +1,5 @@
-
 // import "babel-polyfill"; // https://babeljs.io/docs/usage/polyfill/
-
 // needed?  –  require('babel-core/register');
-console.log( "better");
 
 // needed for all testing
 import array from 'core-js/fn/array';
