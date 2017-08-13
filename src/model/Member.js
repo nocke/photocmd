08611d135a72db02e484df9530140fd3399626b2
Frank <git@nocke.de>
2017-08-13 07:16:52 +0200

    member: add normalized 'type'

--- src/model/Member.js
+++ src/model/Member.js
@@ -7,6 +7,33 @@ import {enforce, fail} from '../helpers';
 	constructor(pathObj) {
 		// copy 'em all
 		Object.assign(this,pathObj);
+
+		// create type
+		switch(this.ext.toLowerCase()) {
+			// regular
+			case 'png':
+			case 'psd':
+			case 'jpg':
+			case 'tif':
+			case 'xmp':
+				this.type=this.ext.toLowerCase;
+				break;
+
+			// special cases
+			case 'jpeg':
+				this.type='jpg';
+				break;
+			case 'tiff':
+				this.type='tif';
+				break;
+			// → en.wikipedia.org/wiki/Raw_image_format
+			case 'crw':
+			case 'cr2':
+			case 'dng':
+			case 'raw':
+				this.type='raw';
+				break;
+		}
 	}
 
 }
