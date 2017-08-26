7af1d6b4118469ee4b5700c3481b30142ce6dfbc
Frank <git@nocke.de>
2017-08-26 14:45:22 +0200

    helpers: added a move function

--- src/helpers.js
+++ src/helpers.js
@@ -2,6 +2,8 @@
  * Franks small set of little helpers.
  * i.e. sanity checks
  */
+var fs = require('fs');
+
 
 export function enforce(expr, msg='enforce failed', ...args) {
 	if( expr !== true )
@@ -15,3 +17,41 @@ export function fail(msg='failing', ...args) {
 }
 
 
+
+/**
+ * A move() function that renames, if possible, or falls back to copying
+ * https://stackoverflow.com/a/29105404/444255
+ * @param {*} oldPath
+ * @param {*} newPath
+ * @param {*} callback
+ *
+ * TOTEST
+ */
+export function move(oldPath, newPath, callback) {
+
+    fs.rename(oldPath, newPath, function (err) {
+        if (err) {
+            if (err.code === 'EXDEV') {
+                copy();
+            } else {
+                callback(err);
+            }
+            return;
+        }
+        callback();
+    });
+
+    function copy() {
+        var readStream = fs.createReadStream(oldPath);
+        var writeStream = fs.createWriteStream(newPath);
+
+        readStream.on('error', callback);
+        writeStream.on('error', callback);
+
+        readStream.on('close', function () {
+            fs.unlink(oldPath, callback);
+        });
+
+        readStream.pipe(writeStream);
+    }
+}
