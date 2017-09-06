b1c46d8f4f2cc21acc93c4a20e5efe30cf9c031d
Frank <git@nocke.de>
2017-09-06 13:42:49 +0200

    add trash.spec.js (WIP)

---
+++ test/trash.spec.js
@@ -0,0 +1,22 @@
+'use strict'
+
+import chai from 'chai';
+import path from 'path';
+
+// shorthand
+const assert = chai.assert;
+
+describe('trash module', () => {
+
+	it('test one', () => {
+
+		assert( true, 'worked' );
+	});
+
+	it('test two', () => {
+
+		assert( false, '(shall fail)' );
+	});
+
+
+});
