981324eb712c9129bf28e06d9c50e34ab03a1ea7
Frank Nocke <frank.nocke.extern@holidaycheck.com>
2017-10-11 19:17:14 +0200

    mocha promise test (not yet working)

--- test/ES6.self-test.spec.js
+++ test/ES6.self-test.spec.js
@@ -6,6 +6,22 @@ import path from 'path';
 // shorthand
 const assert = chai.assert;
 
+// async function x(aPromise) {
+// 	await aPromise
+// }
+
+// a simple promise function
+function get42() {
+	return new Promise(function (fulfill, reject) {
+		console.log('entering...');
+		setTimeout( ()=>{
+			console.log('about to resolve...');
+			resolve(42);
+		}, 500);
+	});
+}
+
+
 describe('ES6 self-test', () => {
 
 	it('Array', () => {
@@ -24,6 +40,23 @@ describe('ES6 self-test', () => {
 		assert( mySet.has(1) );
 		assert( !mySet.has(3) );
 		assert( mySet.size === 2 );
+	});
+
+
+
+	it('Async - Await', () => {
+
+		// await 'Hello';
+		get42()
+		.then((v) =>
+			console.log('now got'+v)
+		)
+		.catch((reason) =>
+			console.error(reason)
+		);
+
+		// TODO testing promise is not really happening
+
 	});
 
 
