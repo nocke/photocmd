ee5bb4d1cef6fa40af9108fb9864eef72a01c28c
Frank Nocke <frank.nocke.extern@holidaycheck.com>
2017-10-16 12:55:54 +0200

    add config, trash TWEEN

---
+++ test/config.js
@@ -0,0 +1,5 @@
+// same basic test config
+export default {
+	testDir : './build/fileTests',
+	test: '1'
+}
--- test/trash.spec.js
+++ test/trash.spec.js
@@ -1,22 +1,38 @@
 'use strict'
 
 import chai from 'chai';
-import path from 'path';
-
-// shorthand
 const assert = chai.assert;
+import path from 'path';
+import fs from 'fs';
+
+import trash from 'trash';
+import config from './config';
+
 
 describe('trash module', () => {
 
-	it('test one', () => {
+	beforeEach(async () => {
+		console.log('beforeEach123');
+		const dir = config.testDir;
+		console.dir(dir);
+
+		if (fs.existsSync(dir)){
+			console.log(`deleting ${dir}`);
+			await fs.rmdir(dir);
+
+		} 
+
+
+	});
+
+	it('test simple deletion', () => {
 
 		assert( true, 'worked' );
+ 
+
+
 	});
 
-	it('test two', () => {
-
-		assert( false, '(shall fail)' );
-	});
 
 
 });
