28afc0de1cd8c2a1b656cf806966a45c10a28bd0
Frank Nocke <frank.nocke.extern@holidaycheck.com>
2018-02-02 17:31:16 +0100

    add styles

--- src/photoui.js
+++ src/photoui.js
@@ -16,7 +16,11 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({
+	  width: 800,
+	  height: 600
+
+	})
 
   // and load the index.html of the app.
   win.loadURL(url.format({
@@ -26,7 +30,7 @@ function createWindow () {
   }))
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  // DISABLE win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
