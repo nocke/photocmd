5033b567d8593cb7d363f270d262449c5dfa1c74
Frank <git@nocke.de>
2018-06-29 07:54:57 +0200

    a better deleteWrap

--- src/deleteAction.js
+++ src/deleteAction.js
@@ -15,30 +15,38 @@ import {
 async function deleteAction(firstDir, moreDirs, cmd) { // TODO: refactor → deleteLonely
 	log('Delete Action ************************')
 
-	let liveMode = cmd.live || false
-	let verboseMode = cmd.verbose || false
+	const liveMode = cmd.live || false
+	const verboseMode = cmd.verbose || false
 	if (verboseMode) setLevel(LEVELS.INFO)
 
-	try {
-		let lonely = cmd.lonely || false
-		let unstarred = cmd.unstarred || false
+	let lonely = cmd.lonely || false
+	let unstarred = cmd.unstarred || false
 
-		// merge all dirs together
-		let dirs = [firstDir, ...moreDirs]
-		enforce(!!dirs, 'no directory specified')
+	// merge all dirs together
+	let dirs = [firstDir, ...moreDirs]
+	enforce(!!dirs, 'no directory specified')
 
-		// TODO: force lonely only   or   starred ony   or  …else…
+	// TODO: force lonely only   or   starred ony   or  …else…
 
-		//loneFiles.dump()
-		let fileSet = new FileSet(dirs)
-		let loneFiles = fileSet.getLonely()
-		await loneFiles.delete(liveMode)
-	} catch(err) {
-		warn('an error occured')
-		if (verboseMode)
-			console.dir(err)
-	}
+	//loneFiles.dump()
+	const fileSet = new FileSet(dirs)
+	const loneFiles = fileSet.getLonely()
+	await loneFiles.delete(liveMode)
 }
 
+function deleteActionWrap(firstDir, moreDirs, cmd) {
 
-export default deleteAction
+	deleteAction(firstDir, moreDirs, cmd)
+	.then( result => {
+			log('confirmation message:')
+			log()
+	})
+	.catch( err => {
+		warn(err.message) // (makes log in error itself superficious)
+		if(cmd.verbose)
+			console.log(err.stack)
+	})
+
+}
+
+export default deleteActionWrap
