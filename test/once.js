4c80a6fc249cc8bff76ffae93654e09e7b34ed48
Frank <git@nocke.de>
2018-12-20 19:45:25 +0100

    choires: better suiteMode detection

--- test/once.js
+++ test/once.js
@@ -1,9 +1,17 @@
-import { mute } from "../src/util/log";
+import { mute, setLevel } from "../src/util/log";
 
-// the global nodejs object with some settings
+// the global nodejs object with certain settings
 import '../src/global'
 
-global.app.suiteMode = process.env.npm_lifecycle_script !== 'mocha $1'
+global.app.suiteMode = !(
+	process.env.npm_lifecycle_script == 'mocha $1' ||
+	process.env.npm_lifecycle_event && process.env.npm_lifecycle_event.includes('test-single')
+)
+
+console.log('running in '+( global.app.suiteMode ? 'suite mode' : 'single mode' ))
 
 // keep verbosity low, when running full suite (only warn() and above)
-if ( global.app.suiteMode ) mute()
+if ( global.app.suiteMode )
+	mute()
+else // by default, we want to read all   TODO: into the globel per-suite setup?
+	setLevel(1)
