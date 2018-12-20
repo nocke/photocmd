ecaf6f21def9e51c31f7b6c28fe5b5b67df13011
Frank <git@nocke.de>
2018-12-20 21:46:59 +0100

    no suiteMode in Debugging, detect vscode debug

--- test/once.js
+++ test/once.js
@@ -1,11 +1,14 @@
+
+import Mocha from 'mocha'
 import { mute, setLevel } from "../src/util/log";
 
 // the global nodejs object with certain settings
 import '../src/global'
 
 global.app.suiteMode = !(
-	process.env.npm_lifecycle_script == 'mocha $1' ||
-	process.env.npm_lifecycle_event && process.env.npm_lifecycle_event.includes('test-single')
+	process.env.VSCODE_IPC_HOOK || // full info on debugging
+	process.env.npm_lifecycle_script == 'mocha $1' || // full info on single run
+	process.env.npm_lifecycle_event && process.env.npm_lifecycle_event.includes('test-single') // lousy backup
 )
 
 console.log('running in '+( global.app.suiteMode ? 'suite mode' : 'single mode' ))
@@ -13,5 +16,5 @@ console.log('running in '+( global.app.suiteMode ? 'suite mode' : 'single mode' 
 // keep verbosity low, when running full suite (only warn() and above)
 if ( global.app.suiteMode )
 	mute()
-else // by default, we want to read all   TODO: into the globel per-suite setup?
+else // by default, we want to read all
 	setLevel(1)
