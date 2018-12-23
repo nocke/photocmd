c210c4bb0b68274c7bec8013cb81437a8e96b2a3
Frank <git@nocke.de>
2018-12-23 16:29:47 +0100

    TWEEN fileSet.test: add helper assert

--- test/FileSet.test.js
+++ test/FileSet.test.js
@@ -9,29 +9,42 @@ import { mockfile, assertFiles } from './_testtools'
 import log, { setLevel, LEVELS, info, warn, error, enforce, fail, snooze, unsnooze } from '../src/util/log'
 
 // test config:
-const testDir = path.resolve( global.app.dirs.test, 'deleteActionTests' )
+const testDir = path.resolve(global.app.dirs.test, 'deleteActionTests')
 
 // system under test:
 import fileUtils from '../src/util/fileUtils'
 import FileSet from '../src/model/FileSet' // testing has to go through FileSet anyway
 
 
+describe('FileSet testing', () => {
 
-describe( 'FileSet testing', () => {
+	// helper function
+	const assertCoreData = (filepath, expected) => {
+		const r = FileSet.parseCoreData(filepath)
+		warn(r)
+		for (const prop in expected) {
+			assert.equal(
+				r[prop],
+				expected[prop],
+				`prop "${prop}" on filepath "${filepath}"`
+			)
+			log(prop)
+		}
 
+	}
 
-	it( 'test getCore - images', () => {
-		assert.equal( FileSet.getCore( {name:'IMG_0634.JpG'}), 'IMG_0634')
+	it('test getCore - images', () => {
+		assert.equal(FileSet.parseCoreData('IMG_0634.JpG'), 'IMG_0634')
 
-		assert.equal( FileSet.getCore( {name:'PM5A2087.cr2'}), 'PM5A2087')
-		assert.equal( FileSet.getCore( {name:'PM5A2087_DXs2.jpg'}), 'PM5A2087')
-		assert.equal( FileSet.getCore( {name:'PM5A2087_02_final.jpg'}), 'PM5A2087')
+		assert.equal(FileSet.parseCoreData('PM5A2087.cr2'), 'PM5A2087')
+		assert.equal(FileSet.parseCoreData('PM5A2087_DXs2.jpg'), 'PM5A2087')
+		assert.equal(FileSet.parseCoreData('PM5A2087_02_final.jpg'), 'PM5A2087')
 
 		// singles
-		assert.equal( FileSet.getCore( {name:'beaches.JpG'}), 'beaches')
+		assert.equal(FileSet.parseCoreData('beaches.JpG'), 'beaches')
 
 		// extensionless (yes, oddity)
-		assert.equal( FileSet.getCore( {name:'PM5A2087'}), 'PM5A2087')
+		assert.equal(FileSet.parseCoreData('PM5A2087'), 'PM5A2087')
 
 		// COULDDO versioned singles
 		// Ipswick123_01
@@ -39,47 +52,23 @@ describe( 'FileSet testing', () => {
 		// Ipswick123_02 with space
 	})
 
-	it( 'test getCore - sidecars', () => {
+	it('test getCore - sidecars', () => {
 		// sidecar instead
-		assert.equal( FileSet.getCore( {name:'IMG_0636.nef'}), 'IMG_0636')
-		assert.equal( FileSet.getCore( {name:'PM5A1234.xmp'}), 'PM5A1234')
-		assert.equal( FileSet.getCore( {name:'Singles.xmp'}), 'Singles')
+		assert.equal(FileSet.parseCoreData('IMG_0636.nef'), 'IMG_0636')
+		assert.equal(FileSet.parseCoreData('PM5A1234.xmp'), 'PM5A1234')
+		assert.equal(FileSet.parseCoreData('Singles.xmp'), 'Singles')
 
 
 		// sidecar append
-		assert.equal( FileSet.getCore( {name:'IMG_0636.jpg.nef'}), 'IMG_0636')
-		assert.equal( FileSet.getCore( {name:'PM5A1234.jpg.xmp'}), 'PM5A1234')
-
+		assert.equal(FileSet.parseCoreData('IMG_0636.jpg.nef'), 'IMG_0636')
+		assert.equal(FileSet.parseCoreData('PM5A1234.jpg.xmp'), 'PM5A1234')
 
 	})
 
-
-	it( 'test getCore - movies', () => {
-		
-		assert.equal( FileSet.getCore( {name:'PM5A2087'}), 'PM5A2087')
+	it.only('test getCore - movies', () => {
+		assertCoreData('PM5A2087.mov', { name: 'PM5A2087', type: 'mov' })
 	})
 
-	it.only( 'temp', () => {
-		
-		assert.equal( FileSet.getCore( {name:'beaches.JpG'}), 'beaches')
-	})
+	// COULDDO 19-hundred?  19\d\d ?
 
-
-		// const fileSet = new FileSet( [ testDir ] )
-		// // log(fileSet._families)
-
-		// const family1 = fileSet._families.get( 'DSCN00248' )
-
-		// assert.equal( family1._core, 'DSCN00248', 'family core name' )
-		// assert.equal( family1._map.size, 5, 'family member count mismatch' )
-		// assert.isFalse( family1._isLonely, 'family not lonely' )
-
-		// const family2 = fileSet._families.get( '2017-10-25_01234567' )
-		// assert.equal( family2._core, '2017-10-25_01234567', 'family core name' )
-		// assert.equal( family2._map.size, 3, 'family member count mismatch' )
-		// assert.isFalse( family2._isLonely, 'family not lonely' )
-
-
-		// COULDDO 19-hundred?  19\d\d ?
-
-} )
+})
